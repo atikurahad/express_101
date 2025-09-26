3e956fefa03c272874e68079e6ac5a560ad01975
atikurahad <atikurahad@gmail.com>
2025-09-26 23:47:52 +0600

    express tutorial

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.get("/about", (req, res) => {
+  res.status(200).send({
+    message: "This is about page  ",
+    description: "I am from about page",
+  });
+});
+
+app.get("/", (req, res) => {
+  fs.readFile("./index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+app.get("/faq", (req, res) => {
+  res.status(200).json([
+    {
+      question: "What is this platform?",
+      answer: "This is for all the people who want to learn Express.js.",
+    },
+    {
+      question: "Is this platform free?",
+      answer: "Yes, it is completely free to use.",
+    },
+    {
+      question: "Can I contribute?",
+      answer:
+        "Yes! You can contribute by submitting tutorials and code examples.",
+    },
+  ]);
+});
+
+app.get("/question", (req, res) => {
+  res.status(200).json({
+    data: [
+      {
+        question: "What is this platform?",
+        answer: "This is for all the people who want to learn Express.js.",
+      },
+      {
+        question: "Is this platform free?",
+        answer: "Yes, it is completely free to use.",
+      },
+      {
+        question: "Can I contribute?",
+        answer:
+          "Yes! You can contribute by submitting tutorials and code examples.",
+      },
+    ],
+  });
+});
+
+
+
+
+app.listen(4000, () => {
+  console.log("Server is running on http://localhost:4000");
+});
